b1b6c49471c95b446ca907ab3203fad97bbd3b92
minhtricute69 <minhtricute69@gmail.com>
2022-06-03 15:58:59 +0700

    Init Commit GCC-8.5

---
+++ lib/gcc/aarch64-elf/8.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf CFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' --prefix=/home/tri/gcc-build/../gcc-arm64 --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=single --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
